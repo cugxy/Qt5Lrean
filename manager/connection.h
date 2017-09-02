6f4f27857aa7e0f86de783da817b8eb599e7b912
cugxy <cugxy@qq.com>
2017-09-02 22:30:46 +0800

    create database

---
+++ manager/connection.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <qsql.h>
+#include <qdebug.h>
+#include <qdom.h>
+
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
+static bool createConnection()
+{
+	QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+
+	db.setDatabaseName("data.db");
+	db.setUserName("cugxy");
+	db.setPassword("123456");
+	if (!db.open())
+	{
+		return false;
+	}
+
+	QSqlQuery query;
+	query.exec("create table factory(id varchar primary key, name varchar)");
+	query.exec("insert into factory values ('0', '请选择厂家')");
+	query.exec("insert into factory values ('01', '一汽大众')");
+	query.exec("insert into factory values ('02', '福特')");
+	query.exec("insert into factory values ('03', '上海大众')");
+
+	query.exec("create table brand(id varchar primary key, name varchar, factory varchar, price int, sum int, sell int, last int)");
+	query.exec("insert into brand values ('01', '奥迪A6', '一汽大众', 46, 34, 19, 39)");
+	query.exec("insert into brand values ('02', '捷达', '一汽大众', 34, 54, 21, 123)");
+	query.exec("insert into brand values ('03', '奔驰', '一汽大众', 66, 64, 76, 23)");
+	query.exec("insert into brand values ('04', '福特野马', '福特', 90, 89, 99, 897)");
+	query.exec("insert into brand values ('05', '老炮', '福特', 426, 754, 72, 57)");
+	query.exec("insert into brand values ('06', '史蒂夫', '福特', 54, 456, 19, 56)");
+	query.exec("insert into brand values ('07', '帕萨特', '上海大众', 73, 67, 512, 123)");
+	query.exec("insert into brand values ('08', '桑塔纳', '上海大众', 72, 23, 19, 451)");
+	query.exec("insert into brand values ('09', '奥额为', '上海大众', 54, 165, 65, 23)");
+}
+
--- manager/main.cpp
+++ manager/main.cpp
@@ -1,8 +1,13 @@
 #include "widget.h"
 #include <QtWidgets/QApplication>
-
+#include "connection.h"
 int main(int argc, char *argv[])
 {
+	if (!createConnection())
+	{
+		return 0;
+	}
+
 	QApplication a(argc, argv);
 	Widget w;
 	w.show();
